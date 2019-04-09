11fc991d5d49b74d3c2cb9959788d3ef30c13794
hellowShang <13473136348@163.com>
2019-04-09 10:09:50 +0000

    修改

--- app/Http/Controllers/WxController.php
+++ app/Http/Controllers/WxController.php
@@ -18,10 +18,14 @@ class WxController extends Controller
     public function wx(){
         // 接收微信消息推送post过来的信息并写入到自定义的log日志中
         $content = file_get_contents('php://input');
+        $arr = json_decode(json_encode($content),true);
+
         $time = date('Y-m-d H:i:s');
         $str = $time.$content."\n";
+        // 检测是否有logs目录，没有就创建
         is_dir('logs') or mkdir('logs',0777,true);
         file_put_contents("logs/wx.log",$str,FILE_APPEND);
+        file_put_contents("logs/test.log",$arr,FILE_APPEND);
 
         // 回应微信
         echo 'success';
@@ -33,9 +37,9 @@ class WxController extends Controller
         $key = 'access_token';
         $token = Redis::get($key);
         if($token){
-            echo 111;
+//            echo 111;
         }else{
-            echo 222;
+//            echo 222;
             // 接口调用请求说明
             $url = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=".env('WX_APPID')."&secret=".env('WX_SECRET');
             // echo $url;
@@ -62,10 +66,11 @@ class WxController extends Controller
         // 获取access_token
         $access_token = $this->getAccessToken();
 
-        //获取用户基本信息
-        $url = 'https://api.weixin.qq.com/cgi-bin/user/info?access_token='.$access_token.'&openid=o9FAg1Fzv1WOYzX8xGinlYQtRMnc&lang=zh_CN';
-//        $response = file_get_contents($url);
-        var_dump($url);
+        echo 'tonke:'.$access_token;
+//        //获取用户基本信息
+//        $url = 'https://api.weixin.qq.com/cgi-bin/user/info?access_token='.$access_token.'&openid=o9FAg1Fzv1WOYzX8xGinlYQtRMnc&lang=zh_CN';
+////        $response = file_get_contents($url);
+//        var_dump($url);
 
     }
 }
