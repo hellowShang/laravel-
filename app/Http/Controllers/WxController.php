f4b0bcb48dc1e8dc0f09420385f3de0e0483e911
hellowShang <13473136348@163.com>
2019-04-09 07:03:16 +0000

    修改

--- app/Http/Controllers/WxController.php
+++ app/Http/Controllers/WxController.php
@@ -6,14 +6,14 @@ use Illuminate\Http\Request;
 
 class WxController extends Controller
 {
-    // 微信第一次接入
+    // 处理微信第一次接入
     public function valid(){
         // 开发者通过检验signature对请求进行校验（下面有校验方式）。
         // 若确认此次GET请求来自微信服务器，请原样返回echostr参数内容，则接入生效，成为开发者成功，否则接入失败。
         echo $_GET['echostr'];
     }
 
-    // 接收微信推送
+    // 接收微信消息推送
     public function wx(){
         // 接收微信消息推送post过来的信息并写入到自定义的log日志中
         $content = file_get_contents('php://input');
@@ -21,7 +21,7 @@ class WxController extends Controller
         $str = $time.$content."\n";
         file_put_contents("logs/wx.log",$str,FILE_APPEND);
 
-        // 回应
+        // 回应微信
         echo 'success';
     }
 
