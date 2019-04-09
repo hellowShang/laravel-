1e1eeaba4a2d1e8aca2f09c564f9d95b456175eb
hellowShang <13473136348@163.com>
2019-04-09 14:57:44 +0800

    修改首页

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    大哥真帅
                 </div>
 
                 <div class="links">
