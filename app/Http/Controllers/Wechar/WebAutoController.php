ba4028bebe51618335095ba217376ceda21dc675
hellowShang <13473136348@163.com>
2019-04-24 12:18:09 +0000

    微信网页授路径

--- app/Http/Controllers/Wechar/WebAutoController.php
+++ app/Http/Controllers/Wechar/WebAutoController.php
@@ -9,9 +9,9 @@ class WebAutoController extends Controller
 {
     // 微信网页授权
     public function webUrl(){
-        dd(request()->all());
+        dd($_GET);
         $code = $_GET['code'];
-        $url = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=".env('WX_APPID')."&redirect_uri=http%3A%2F%2Fwechar.lab993.com%2Fwechar%2Furl&response_type=".$code."&scope=snsapi_userinfo&state=STATE#wechat_redirect";
+        $url = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=".env('WX_APPID')."&redirect_uri=http%3A%2F%2Fwechar.lab993.com%2Fwechar%2Fauto&response_type=".$code."&scope=snsapi_userinfo&state=STATE#wechat_redirect";
         $response = json_decode(file_get_contents($url),true);
         dd($response);
     }
