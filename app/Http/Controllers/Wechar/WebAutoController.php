0ce553ad3e6694fdfc5ba81dd907dd78abef8b4d
hellowShang <13473136348@163.com>
2019-04-24 12:31:35 +0000

    获取code

--- app/Http/Controllers/Wechar/WebAutoController.php
+++ app/Http/Controllers/Wechar/WebAutoController.php
@@ -10,7 +10,7 @@ class WebAutoController extends Controller
     // 微信网页授权
     public function webUrl(){
         $code = $_GET['code'];
-        $url = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=".env('WX_APPID')."&redirect_uri=http%3A%2F%2Fwechar.lab993.com%2Fwechar%2Fauto&response_type=".$code."&scope=snsapi_userinfo&state=STATE#wechat_redirect";
+        $url = "https://api.weixin.qq.com/sns/oauth2/access_token?appid=".env('WX_APPID')."&secret=".env('WX_SECRET')."&code=$code&grant_type=authorization_code";
         $response = json_decode(file_get_contents($url),true);
         dd($response);
     }
