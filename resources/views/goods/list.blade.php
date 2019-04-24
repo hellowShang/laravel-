1b6cb97ee343ed3b2a7870a628f74b807f735519
hellowShang <13473136348@163.com>
2019-04-24 03:44:29 +0000

    debug

--- resources/views/goods/list.blade.php
+++ resources/views/goods/list.blade.php
@@ -21,9 +21,7 @@
 @endforeach
 
 <script src="http://res.wx.qq.com/open/js/jweixin-1.4.0.js"></script>
-<script src="/js/jquery.js"></script>
 <script>
-    $(function(){
         // 通过config接口注入权限验证配置
         wx.config({
             debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
@@ -48,7 +46,6 @@
                     },
                 });
             });
-        });
 </script>
 </body>
 </html>
