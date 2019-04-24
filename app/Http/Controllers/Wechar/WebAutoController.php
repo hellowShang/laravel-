50913c2c13ab4d84defe9ea96ada276059a32130
hellowShang <13473136348@163.com>
2019-04-24 13:32:59 +0000

    数据入库

--- app/Http/Controllers/Wechar/WebAutoController.php
+++ app/Http/Controllers/Wechar/WebAutoController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Wechar;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Model\Wechar\WecharModel;
 
 class WebAutoController extends Controller
 {
@@ -14,8 +15,62 @@ class WebAutoController extends Controller
         $response = json_decode(file_get_contents($url),true);
         $token = $response['access_token'];
         $openid = $response['openid'];
-        $urli = "https://api.weixin.qq.com/sns/userinfo?access_token=$token&openid=$openid&lang=zh_CN";
-        $responses = json_decode(file_get_contents($urli),true);
-        dd($responses);
+        $url1 = "https://api.weixin.qq.com/sns/auth?access_token=$token&openid=$openid";
+        $response1 = json_decode(file_get_contents($url1),true);
+
+        $time = time();
+        if($response1['errcode'] == 0){
+            $url2 = "https://api.weixin.qq.com/sns/userinfo?access_token=$token&openid=$openid&lang=zh_CN";
+            $response2 = json_decode(file_get_contents($url2),true);
+
+            // 查询当前openID数据库是否存在
+            $arr = WecharModel::where(['openid' => $openid])->first();
+
+            if ($arr) {
+                // 已入库，消息回复
+                $message = "<xml>
+                            <ToUserName><![CDATA[".env('WX_APPID')."]]></ToUserName>
+                            <FromUserName><![CDATA[$openid]]></FromUserName>
+                            <CreateTime>$time</CreateTime>
+                            <MsgType><![CDATA[text]]></MsgType>
+                            <Content><![CDATA[欢迎登录，" . $arr['nickname'] . "]]></Content>
+                        </xml>";
+            } else {
+                // 首次关注，消息入库
+                $info = [
+                    'sub_status' => 1,
+                    'openid' => $response2['openid'],
+                    'nickname' => $response2['nickname'],
+                    'sex' => $response2['sex'],
+                    'city' => $response2['city'],
+                    'province' => $response2['province'],
+                    'country' => $response2['country'],
+                    'headimgurl' => $response2['headimgurl'],
+                ];
+
+                // 数据入库
+                $res = WecharModel::insert($info);
+                if ($res) {
+                    // 消息回复
+                    $message = "<xml>
+                                <ToUserName><![CDATA[".env('WX_APPID')."]]></ToUserName>
+                                <FromUserName><![CDATA[$openid]]></FromUserName>
+                                <CreateTime>$time</CreateTime>
+                                <MsgType><![CDATA[text]]></MsgType>
+                                <Content><![CDATA[你好" . $response2['nickname'] . "，欢迎登录]]></Content>
+                            </xml>";
+                }
+            }
+        }else{
+            // 消息回复
+            $message = "<xml>
+                                <ToUserName><![CDATA[".env('WX_APPID')."]]></ToUserName>
+                                <FromUserName><![CDATA[$openid]]></FromUserName>
+                                <CreateTime>$time</CreateTime>
+                                <MsgType><![CDATA[text]]></MsgType>
+                                <Content><![CDATA[出错了]]></Content>
+                            </xml>";
+        }
+
     }
 }
