d001b808e15de7811edc40521f6de28c92ebab68
hellowShang <13473136348@163.com>
2019-04-24 13:18:23 +0000

    获取用户信息

--- app/Http/Controllers/Wechar/WebAutoController.php
+++ app/Http/Controllers/Wechar/WebAutoController.php
@@ -12,6 +12,10 @@ class WebAutoController extends Controller
         $code = $_GET['code'];
         $url = "https://api.weixin.qq.com/sns/oauth2/access_token?appid=".env('WX_APPID')."&secret=".env('WX_SECRET')."&code=$code&grant_type=authorization_code";
         $response = json_decode(file_get_contents($url),true);
-        dd($response);
+        $token = $response['refresh_token'];
+        $openid = $response['openid'];
+        $urli = "https://api.weixin.qq.com/sns/userinfo?access_token=$token&openid=$openid&lang=zh_CN";
+        $responses = json_decode(file_get_contents($urli),true);
+        dd($responses);
     }
 }
