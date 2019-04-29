05624c18cee4e7bbca5f60242057b8fe6723fdc2
hellowShang <13473136348@163.com>
2019-04-29 12:20:26 +0000

    代码注释

--- app/Http/Controllers/Wechar/WebAutoController.php
+++ app/Http/Controllers/Wechar/WebAutoController.php
@@ -31,37 +31,39 @@ class WebAutoController extends Controller
             $url2 = "https://api.weixin.qq.com/sns/userinfo?access_token=$token&openid=$openid&lang=zh_CN";
             $response2 = json_decode(file_get_contents($url2), true);
 
-//            // 查询当前openID数据库是否存在
-//            $arr = WecharModel::where(['openid' => $openid])->first();
-//
-//            if ($arr) {
-//                // 已入库，消息回复
-//                $message = "<script>alert('欢迎登录，".$arr['nickname']."，3秒后跳转至福利页面')</script>";
-//            } else {
-//                // 首次登录，数据入库
-//                $info = [
-//                    'sub_status' => 1,
-//                    'openid' => $response2['openid'],
-//                    'nickname' => $response2['nickname'],
-//                    'sex' => $response2['sex'],
-//                    'city' => $response2['city'],
-//                    'province' => $response2['province'],
-//                    'country' => $response2['country'],
-//                    'headimgurl' => $response2['headimgurl'],
-//                ];
-//
-//                // 数据入库
-//                $res = WecharModel::insert($info);
-//                if ($res) {
-//                    // 消息回复
-//                    $message = "<script>alert('你好" . $response2['nickname'] . "，欢迎登录，3秒后跳转至福利页面')</script>";
-//                }
-//            }
-//
-//
-//        }else{
-//            // 出错消息回复
-//            $message = "<script>alert('出错了')</script>";
+            /*
+            // 查询当前openID数据库是否存在
+            $arr = WecharModel::where(['openid' => $openid])->first();
+
+            if ($arr) {
+                // 已入库，消息回复
+                $message = "<script>alert('欢迎登录，".$arr['nickname']."，3秒后跳转至福利页面')</script>";
+            } else {
+                // 首次登录，数据入库
+                $info = [
+                    'sub_status' => 1,
+                    'openid' => $response2['openid'],
+                    'nickname' => $response2['nickname'],
+                    'sex' => $response2['sex'],
+                    'city' => $response2['city'],
+                    'province' => $response2['province'],
+                    'country' => $response2['country'],
+                    'headimgurl' => $response2['headimgurl'],
+                ];
+
+                // 数据入库
+                $res = WecharModel::insert($info);
+                if ($res) {
+                    // 消息回复
+                    $message = "<script>alert('你好" . $response2['nickname'] . "，欢迎登录，3秒后跳转至福利页面')</script>";
+                }
+            }
+
+
+        }else{
+            // 出错消息回复
+            $message = "<script>alert('出错了')</script>";
+            */
         }
 //        echo $message;
             $id = rand(11,99);
