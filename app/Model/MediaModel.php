05fb3622c20f7002be046b006b7ce46870babf32
hellowShang <13473136348@163.com>
2019-04-29 03:36:26 +0000

    素材表

--- app/Model/MediaModel.php
+++ app/Model/MediaModel.php
@@ -6,6 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class MediaModel extends Model
 {
-    protected $table = 'wechar_media';
+    protected $table = 'wechar_medias';
     public $timestamps = false;
 }
